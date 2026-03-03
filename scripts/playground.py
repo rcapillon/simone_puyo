822be2c6559236a83c6018dda98d914ac3831cd4
rcapillon <rcapillon@yahoo.fr>
2026-03-03 11:48:37 +0100

    changed reward scaling

--- scripts/playground.py
+++ scripts/playground.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     puyo_game = PuyoGame(max_moves=max_moves)
 
     mcts_config = MCTSConfig(
-        n_simulations=1000,
+        n_simulations=400,
         UCT_exploration_constant=1.5,
         discount_factor=0.99,
         dirichlet_alpha=0.3,
